068ba75538f8f996d0b7adea63aaa652f6306fff
Iliyan Dimitrov <18351905+IliyanID@users.noreply.github.com>
2020-11-02 17:14:35 -0700

    WebServer Functions
    WebServer can recieve and send basic text through body on postman

--- src/main/java/RestfulServer.java
+++ src/main/java/RestfulServer.java
@@ -1,3 +1,4 @@
+import spark.Spark.*;
 import spark.Spark;
 import spark.Request;
 import spark.Response;
@@ -6,35 +7,64 @@ public class RestfulServer
 {
     public RestfulServer()
     {
-
+        this.configureRestfulApiServer();
+        this.processRestfulApiRequests();
     }
 
     private void configureRestfulApiServer()
     {
-        spark.port(8080);
+        Spark.port(8080);
         System.out.println("Server configured to listen on port 8080");
+        Spark.get("/hello", (request, response) -> "Hello World!");
     }
 
     private void processRestfulApiRequests()
     {
-        Spark.get(path:"/", this::echoRequest);
+        Spark.get("/",this::echoRequest);
 
     }
 
     private String echoRequest(Request request, Response response)
     {
-        response.type(contentType:"application/json");
-        response.header(header:"Acess-Control-Allow-Origin", value:"*");
-        response.status(statusCode: 200);
+        response.type("application/json");
+        response.header("Access-Control-Allow-Origin","*");
+        response.status(200); //Success
 
-        return "";
+        return HttpRequestToJson(request);
 
     }
 
     private String HttpRequestToJson(Request request)
     {
-
+        return(
+            "{\n"
+            + "\"attributes\":\"" + request.attributes() + "\",\n"
+            + "\"body\":\"" + request.body() +"\" \n"
+            + "\"contentLength\":\"" + request.contentLength() + "\",\n"
+            + "\"contentType\":\"" + request.contentType() + "\", \n"
+            + "\"contextPath\":\"" + request.contextPath() + "\",\n"
+            + "\"cookies\":\"" + request.cookies() + "\",\n"
+            + "\"headers\":\"" + request.headers() + "\" ,\n"
+            + "\"host\":\"" + request.host() + "\", \n"
+            + "\"ip\":\"" + request.ip() + "\", \n"
+            + "\"params\":\"" + request.params() +"\",\n"
+            + "\"pathInfo\":\"" + request.pathInfo() + "\"\n"
+            + "\"serverPort\":\"" + request.port() + "\", \n"
+            + "\"protocol\":\"" + request.protocol() + "\", \n"
+            + "\"queryParams\":\"" + request.queryParams() + "\",\n"
+            + "\"requestMethod\":\"" + request.requestMethod() + "\",\n"
+            + "\"scheme\":\"" + request.scheme() + "\", \n"
+            + "\"servletPath\":\"" + request.servletPath() + "\",\n"
+            + "\"session\":\"" + request.session() + "\",\n"
+            + "\"uri\":\"" + request.uri() + "\",\n"
+            + "\"url\":\"" + request.url() + "\",\n"
+            + "\"userAgent\":\"" + request.userAgent() + "\",\n"
+            + "}");
     }
 
+    public static void main(String[] args)
+    {
+        RestfulServer restfulServer = new RestfulServer(); //This should never return.
+    }
 
 }
